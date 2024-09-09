f264287e834129a4616d4f5bd4c53cb608d16522
msandula12 <msandula@gmail.com>
2024-09-09 15:05:01 -0400

    Basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
-import "./App.css";
+import { FormEvent, SyntheticEvent, useState } from "react";
+
+import styles from "./App.module.scss";
 
 function App() {
+  const [searchValue, setSearchValue] = useState<string>("");
+
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+    console.log(`Search: ${searchValue}`);
+  }
+
+  function updateSearchValue({
+    currentTarget: { value },
+  }: SyntheticEvent<HTMLTextAreaElement>) {
+    setSearchValue(value);
+  }
+
   return (
-    <>
-      <h1>FontFinder</h1>
-    </>
+    <div className={styles.app}>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <h1>FontFinder</h1>
+        <textarea
+          onChange={updateSearchValue}
+          placeholder={`Describe the kind of font(s) you're looking for (e.g., "A bold, rounded font that looks good in all caps")`}
+          rows={4}
+          value={searchValue}
+        />
+        <button>Submit</button>
+      </form>
+    </div>
   );
 }
 
