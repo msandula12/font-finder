83113349a0cd45cfc4b32055fd07e4da88b93e03
msandula12 <msandula@gmail.com>
2024-09-10 15:44:35 -0400

    Load and display Google fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,50 @@
 import { FormEvent, SyntheticEvent, useState } from "react";
+import { BiLinkExternal } from "react-icons/bi";
+import { BiSearch } from "react-icons/bi";
+
+import { GOOGLE_FONTS_CSS_API, GOOGLE_FONTS_FONT_URL } from "./constants";
 
 import styles from "./App.module.scss";
 
-const GOOGLE_FONTS_API = "https://www.googleapis.com/webfonts/v1/webfonts";
 const GOOGLE_FONTS_KEY = import.meta.env.VITE_GOOGLE_FONTS_API_KEY;
+const MAX_INPUT_LENGTH = 1000;
 
-const fontFamily = "Roboto";
+const TEST_FONTS = [
+  "Roboto",
+  "Noto Serif",
+  "Protest Guerrilla",
+  "Sevillana",
+  "Inconsolata",
+];
 
 function App() {
+  const [fonts] = useState<string[]>(TEST_FONTS);
   const [searchValue, setSearchValue] = useState<string>("");
 
   async function fetchFonts() {
+    const fontsQueryString = fonts
+      .map((font) => `family=${font.replace(/\s/g, "+")}`)
+      .join("&");
+
     const response = await fetch(
-      `${GOOGLE_FONTS_API}?key=${GOOGLE_FONTS_KEY}&family=${fontFamily}`
+      `${GOOGLE_FONTS_CSS_API}?key=${GOOGLE_FONTS_KEY}&${fontsQueryString}`
     );
 
     if (!response.ok) {
       throw new Error(`Response status: ${response.status}`);
     }
 
-    const data = await response.json();
+    const fontCss = await response.text();
 
-    console.log("data: ", data.items);
+    const googleFontStyles = document.getElementById("google-font-styles");
+
+    if (googleFontStyles) {
+      googleFontStyles.textContent = fontCss;
+    }
   }
 
   function handleSubmit(event: FormEvent) {
     event.preventDefault();
-    console.log(`Search: ${searchValue}`);
     fetchFonts();
   }
 
@@ -41,13 +59,45 @@ function App() {
       <form className={styles.form} onSubmit={handleSubmit}>
         <h1>FontFinder</h1>
         <textarea
+          className={styles.textarea}
+          maxLength={MAX_INPUT_LENGTH}
           onChange={updateSearchValue}
-          placeholder={`Describe the kind of font(s) you're looking for (e.g., "A bold, rounded font that looks good in all caps")`}
+          placeholder={`Describe the kind of fonts you're looking for (e.g., "Bold, rounded fonts that looks good in all caps")`}
           rows={4}
           value={searchValue}
         />
-        <button>Submit</button>
+        <div className={styles.toolbar}>
+          <span>
+            {searchValue.length} / {MAX_INPUT_LENGTH}
+          </span>
+          <button className={styles.button} disabled={!searchValue.length}>
+            <BiSearch /> Find
+          </button>
+        </div>
       </form>
+      <div className={styles.fonts}>
+        {fonts.map((font) => (
+          <div className={styles.font} key={font} style={{ fontFamily: font }}>
+            <h4 className={styles.fontName}>
+              {font}
+              <a
+                className={styles.fontLink}
+                href={`${GOOGLE_FONTS_FONT_URL}/${font}`}
+                target="_blank"
+              >
+                <BiLinkExternal />
+              </a>
+            </h4>
+            <p>
+              Everyone has the right to freedom of thought, conscience and
+              religion; this right includes freedom to change his religion or
+              belief, and freedom, either alone or in community with others and
+              in public or private, to manifest his religion or belief in
+              teaching, practice, worship and observance.
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/constants.ts
@@ -0,0 +1,2 @@
+export const GOOGLE_FONTS_CSS_API = "https://fonts.googleapis.com/css2";
+export const GOOGLE_FONTS_FONT_URL = "https://fonts.google.com/specimen";
